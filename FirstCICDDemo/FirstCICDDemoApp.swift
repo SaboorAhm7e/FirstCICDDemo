4ab753fb9d3ec4f68df275f9f3d3c55292a744bf
Saboor Ahmad <saboorahm7e@gmail.com>
2024-10-06 10:56:17 +0500

    Initial Commit

---
+++ FirstCICDDemo/FirstCICDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstCICDDemoApp.swift
+//  FirstCICDDemo
+//
+//  Created by Saboor on 06/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FirstCICDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
